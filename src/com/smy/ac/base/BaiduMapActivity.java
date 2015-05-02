f55f1504141cd0eb40ba6a64b1980f34a97ea997
18840850852 <2507826897@qq.com>
2015-05-02 21:52:40 +0800

    '完善BaseActivity'

---
+++ src/com/smy/ac/base/BaiduMapActivity.java
@@ -0,0 +1,82 @@
+package com.smy.ac.base;
+
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+
+/**
+ * 百度地图Activity基类，jar包和一些环境还没有配置
+ * @author smy
+ *
+ */
+public class BaiduMapActivity extends BaseActivity {
+	
+	protected MapView mMapView = null; //地图mapview控件
+	protected BaiduMap map; 
+	
+	protected long centerLong,centerLan; //
+	
+	private int markerRes; //标记物的资源id
+
+/*	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// 在使用SDK各组件之前初始化context信息，传入ApplicationContext
+		// 注意该方法要再setContentView方法之前实现
+		setContentView(R.layout.baidumap_test);
+		// 获取地图控件引用
+		mMapView = (MapView) findViewById(R.id.bmapView);
+	}*/
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// 在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管�?
+		mMapView.onDestroy();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// 在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管�?
+		mMapView.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// 在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管�?
+		mMapView.onPause();
+	}
+	
+	/**
+	 * 在地图上添加标记
+	 * @param latitude 经度
+	 * @param longitude 纬度
+	 */
+	public void addMarker(long latitude,long longitude){
+		LatLng point = new LatLng(latitude, longitude);
+		BitmapDescriptor bmp = BitmapDescriptorFactory.fromResource(markerRes);
+		OverlayOptions option = new MarkerOptions().position(point).icon(bmp);
+		mMapView.getMap().addOverlay(option);
+	}
+	public void setMarkerRes(int markerRes) {
+		this.markerRes = markerRes;
+	}
+
+	@Override
+	protected void initData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initView() {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- src/com/smy/ac/base/BaseActivity.java
+++ src/com/smy/ac/base/BaseActivity.java
@@ -2,9 +2,13 @@ package com.smy.ac.base;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 public abstract class BaseActivity extends Activity {
 
@@ -13,14 +17,23 @@ public abstract class BaseActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 	}
-	
-	/**sub class can override this func not old onCreate to simpleify manipulation*/
-	protected void onCreate(Bundle savedInstanceState,int resId){
+
+	/**
+	 * sub class can override this func not old onCreate to simpleify
+	 * manipulation
+	 */
+	protected void onCreate(Bundle savedInstanceState, int resId) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE); // 设置无标题栏
 		setContentView(resId);
+		/*
+		 * getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		 * WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		 */// 设置全屏
 		init();
 	}
-	protected void onCreate(Bundle savedInstanceState,View view){
+
+	protected void onCreate(Bundle savedInstanceState, View view) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(view);
@@ -44,29 +57,54 @@ public abstract class BaseActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onDestroy();
 	}
-	
-	protected abstract void initData(); //鍒濆鍖栨暟鎹�
-	protected abstract void initView(); //鍒濆鍖栬鍥�
+
+	protected abstract void initData(); // 鍒濆鍖栨暟鎹�
+
+	protected abstract void initView(); // 鍒濆鍖栬鍥�
+
 	protected void init() {
 		initData();
 		initView();
 	}
+
 	// ==findViewById()
-	protected View getView(int resId){
+	protected View getView(int resId) {
 		return findViewById(resId);
 	}
-	
-	protected void goTo(Class<? extends Activity> destActivity,Bundle bundle) {
-		Intent intent = new Intent(this,destActivity);
+
+	protected void goTo(Class<? extends Activity> destActivity, Bundle bundle) {
+		Intent intent = new Intent(this, destActivity);
 		intent.putExtras(bundle);
 		startActivity(intent);
 	}
-	protected void goTo(Class<? extends Activity> destActivity){
-		goTo(destActivity,null);
+
+	protected void goTo(Class<? extends Activity> destActivity) {
+		goTo(destActivity, null);
 	}
-	protected Bundle getBundleFromUp(){
+
+	protected Bundle getBundleFromUp() {
 		return getIntent().getExtras();
 	}
+
+	/*** 获取当前应用程序的版本号*/
+	private String getVersion() {
+		PackageManager pm = getPackageManager();
+		try {
+			PackageInfo packinfo = pm.getPackageInfo(getPackageName(), 0);
+			String version = packinfo.versionName;
+			return version;
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+			return "0"; //0代表未能找到版本号
+		}
+	}
 	
+	/**弹出Toast，Toast的样式今后可以自定义，暂时使用默认*/
+    protected void showMsg(String msg) {
+    	Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();	
+	}
+    protected void showMsg(String msg,int duration){
+    	Toast.makeText(this, msg, duration).show();
+    }
 
 }
